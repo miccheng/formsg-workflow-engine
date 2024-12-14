737580ab1a657fdff4e2a85ebe2050a720929cd5
Michael Cheng <michael_cheng@tech.gov.sg>
2024-12-14 14:40:31 +0800

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/XlSDbdePJj

---
+++ apps/formsg-workflow-engine/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'formsg-workflow-engine',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/formsg-workflow-engine'
+};
