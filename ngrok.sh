3f4c73f34f8163a46c03db6186aa695becf56b07
Michael Cheng <mcheng.work@gmail.com>
2024-12-15 18:25:36 +0800

    chore: Move the Ngrok to a Docker Compose file for ease of expanding into more supporting services.

--- ngrok.sh
+++ ngrok.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -e NGROK_AUTHTOKEN=${NGROK_AUTHTOKEN} -p 4040:4040 ngrok/ngrok:latest http --url=${NGROK_DOMAIN:-regular-lucky-coral.ngrok-free.app} host.docker.internal:3000
+# docker run -it --rm -e NGROK_AUTHTOKEN=${NGROK_AUTHTOKEN} -p 4040:4040 ngrok/ngrok:latest http --url=${NGROK_DOMAIN:-regular-lucky-coral.ngrok-free.app} host.docker.internal:3000
+docker compose up ngrok
