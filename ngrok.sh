727b10e1f2560c4df4aed3cbd5f7b75da259015d
Michael Cheng <mcheng.work@gmail.com>
2024-12-15 17:59:27 +0800

    chore: Clean up the Ngrok container once we close the task

--- ngrok.sh
+++ ngrok.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -e NGROK_AUTHTOKEN=${NGROK_AUTHTOKEN} -p 4040:4040 ngrok/ngrok:latest http --url=${NGROK_DOMAIN:-regular-lucky-coral.ngrok-free.app} host.docker.internal:3000
+docker run -it --rm -e NGROK_AUTHTOKEN=${NGROK_AUTHTOKEN} -p 4040:4040 ngrok/ngrok:latest http --url=${NGROK_DOMAIN:-regular-lucky-coral.ngrok-free.app} host.docker.internal:3000
