af9e521c14bcc73fcef017450728b47d8903e7d9
Michael Cheng <mcheng.work@gmail.com>
2024-12-21 20:45:00 +0800

    feat(FW-5): Implement worker

--- apps/temporal-worker/src/main.ts
+++ apps/temporal-worker/src/main.ts
@@ -1,1 +1,30 @@
-console.log('Hello World');
+import { Worker } from '@temporalio/worker';
+import * as activities from '../../../libs/temporal-workflows/src/lib/all-activities';
+
+async function run() {
+  // Step 1: Register Workflows and Activities with the Worker and connect to
+  // the Temporal server.
+  const worker = await Worker.create({
+    workflowsPath: require.resolve(
+      '/Users/miccheng/Personal/formsg-workflow-engine/dist/libs/temporal-workflows/src/lib/all-workflows.js'
+    ),
+    activities,
+    taskQueue: 'formsg-workflow-engine',
+  });
+  // Worker connects to localhost by default and uses console.error for logging.
+  // Customize the Worker by passing more options to create():
+  // https://typescript.temporal.io/api/classes/worker.Worker
+
+  // If you need to configure server connection parameters, see the mTLS example:
+  // https://github.com/temporalio/samples-typescript/tree/main/hello-world-mtls
+
+  // Step 2: Start accepting tasks on the `monorepo` queue
+  await worker.run();
+
+  // You may create multiple Workers in a single process in order to poll on multiple task queues.
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
