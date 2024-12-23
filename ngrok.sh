904623c418ccebd49d7b3431935559679dfae2a3
Michael Cheng <mcheng.work@gmail.com>
2024-12-23 14:22:34 +0800

    chore: No need for this bash script as we move Ngrok behind the Docker Compose.

--- ngrok.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# docker run -it --rm -e NGROK_AUTHTOKEN=${NGROK_AUTHTOKEN} -p 4040:4040 ngrok/ngrok:latest http --url=${NGROK_DOMAIN:-regular-lucky-coral.ngrok-free.app} host.docker.internal:3000
-docker compose up ngrok
